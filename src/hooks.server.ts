670ddaac2f895c4df673cb704914ec922450c8a0
Zachary Stence <zachstence@gmail.com>
2024-05-05 17:37:12 -0500

    feat: create dummy userId in cookie

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,8 @@ import { Server as HttpsServer } from 'https';
 import { useServer } from 'vite-sveltekit-node-ws';
 
 import { SocketServer, gameService } from '$lib/server';
+import { Handle } from '@sveltejs/kit';
+import { nanoid } from 'nanoid';
 
 useServer(
 	(server) => {
@@ -10,3 +12,15 @@ useServer(
 	},
 	(path) => path.startsWith('/ws')
 );
+
+export const handle: Handle = async ({ event, resolve }) => {
+	let userId = event.cookies.get('userId');
+	if (!userId) {
+		userId = nanoid();
+		event.cookies.set('userId', userId, { path: '/' });
+	}
+
+	event.locals.user = { id: userId };
+
+	return resolve(event);
+};
